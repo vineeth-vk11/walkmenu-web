ab0d711fd60dc8fc579f43fc4f31eafd35a9b147
balajirao <f20180233@hyderabad.bits-pilani.ac.in>
2020-07-29 17:10:21 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# walkmenu-web
---
+++ counter/counter.js
@@ -0,0 +1,9 @@
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>	
+<script type="text/javascript" src="jquery.charactercounter.js"></script>
+
+$(document).ready(function ($) {
+  $(".count-number").counterUp({
+    delay: 10,
+    time: 10000,
+  });
+});
